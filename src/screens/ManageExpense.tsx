0e4289a75751190f8f4208849ca2a7d99a7bedb4
Mikwuegbu <mstephen130@yahoo.com>
2025-01-30 18:42:00 +0100

    folders-structure

---
+++ src/screens/ManageExpense.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const ManageExpense = () => {
+	return <Text>All expenses</Text>;
+};
+
+export default ManageExpense;
---
+++ src/screens/RecentExpense.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const RecentExpense = () => {
+	return <Text>All expenses</Text>;
+};
+
+export default RecentExpense;
