881923304ad0c3b93707abe0bd723817f37fbaa7
Mikwuegbu <mstephen130@yahoo.com>
2025-02-10 18:29:14 +0100

    Remove unused date formatting utility

--- src/util/date.js
+++
@@ -1,4 +0,0 @@
-export const getFormattedDate = (date) => {
-	const options = { year: 'numeric', month: 'long', day: 'numeric' };
-	return new Intl.DateTimeFormat('en-US', options).format(date);
-};
